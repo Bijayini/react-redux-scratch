33c4c44c1b928b1c205dfeeca6330c832c4d9c02
Bijayini <bijayini.parhi@gmail.com>
2018-03-12 18:26:29 +0530

    #000 | Added initial set up for React

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component{
+    render(){
+        return(
+            <div className="container">
+                Hello.. Your Basic react setup is done.
+            </div>
+        );
+    }
+}
